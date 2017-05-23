850409ec71a2bb6840238c3a4ae35bda1eb64984
Jeff Straney <straneyjeff@gmail.com>
2017-05-23 13:50:16 -0400

    created new scripts which will recieve POST requests from the form-partials

---
+++ create.php
@@ -0,0 +1,15 @@
+<?php
+// check the referer to make sure the request was made from this domain
+// This is to prevent CSRF attacks.
+
+// check the request method to make sure it was a POST request
+
+// redirect if the domain is incorrect, or if the method is GET
+
+// get the $_POST parameters
+
+// include the entity controller class
+
+// call the proper method on the object 
+
+?>
