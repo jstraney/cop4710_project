293eb573c3379090cb35d01ddcac269a0cda1fc8
imlasky <ian.lasky1@gmail.com>
2017-05-24 12:04:11 -0400

    added more tables

--- event.sql
+++ event.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: May 24, 2017 at 03:17 PM
+-- Generation Time: May 24, 2017 at 06:03 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -25,17 +25,75 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `user_name` varchar(30) NOT NULL,
+  `comment` varchar(140) NOT NULL,
+  `event` varchar(30) NOT NULL,
+  `comment_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `events`
 --
 
 CREATE TABLE `events` (
   `event_name` varchar(30) NOT NULL,
-  `event_location` varchar(30) NOT NULL,
+  `event_street` varchar(30) NOT NULL,
   `event_time` time NOT NULL DEFAULT '12:00:00',
   `event_org` varchar(30) NOT NULL,
   `event_long` double DEFAULT NULL,
   `event_lat` double DEFAULT NULL,
-  `event_id` int(10) UNSIGNED NOT NULL
+  `event_loc_name` varchar(30) NOT NULL,
+  `event_city` varchar(30) NOT NULL,
+  `event_zip` int(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `RSOs`
+--
+
+CREATE TABLE `RSOs` (
+  `rso_name` varchar(30) NOT NULL,
+  `rso_admin` varchar(30) NOT NULL,
+  `member_count` int(10) UNSIGNED NOT NULL,
+  `rso_id` int(10) UNSIGNED NOT NULL,
+  `university` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `first_name` varchar(30) NOT NULL,
+  `last_name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  `university` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `universities`
+--
+
+CREATE TABLE `universities` (
+  `uni_name` varchar(30) NOT NULL,
+  `uni_street` varchar(60) NOT NULL,
+  `uni_state` char(2) NOT NULL,
+  `uni_zip` int(5) NOT NULL,
+  `uni_num_students` int(10) UNSIGNED NOT NULL,
+  `uni_id` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -43,20 +101,59 @@ CREATE TABLE `events` (
 --
 
 --
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`);
+
+--
 -- Indexes for table `events`
 --
 ALTER TABLE `events`
-  ADD PRIMARY KEY (`event_id`);
+  ADD PRIMARY KEY (`event_time`,`event_loc_name`);
+
+--
+-- Indexes for table `RSOs`
+--
+ALTER TABLE `RSOs`
+  ADD PRIMARY KEY (`rso_id`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- Indexes for table `universities`
+--
+ALTER TABLE `universities`
+  ADD PRIMARY KEY (`uni_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `events`
+-- AUTO_INCREMENT for table `comments`
 --
-ALTER TABLE `events`
-  MODIFY `event_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `RSOs`
+--
+ALTER TABLE `RSOs`
+  MODIFY `rso_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `universities`
+--
+ALTER TABLE `universities`
+  MODIFY `uni_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
