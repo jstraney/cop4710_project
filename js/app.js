acafc6e6680e9b700f72ebccc96d32def000adc5
Jeff Straney <straneyjeff@gmail.com>
2017-05-26 13:59:35 -0400

    added the jquery location plugin to 'event/new'.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
-// global app project
-var app = {};
+var app = app || {};
 
 // add methods to the app object to build a front-end controller
 (function (a) {
 
+  a.makePicker = function () {
+
+    $("#map").locationpicker({
+      enableAutocomplete: true,
+      radius: 0,
+      inputBinding: {
+        longitudeInput: $("#lon"),
+        latitudeInput: $("#lat"),
+        locationNameInput: $("#location"),
+      }
+    });
+  };
 
 })(app);
