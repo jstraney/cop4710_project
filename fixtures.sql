a3d3455b289155e47b40b7411557911485c9c491
Jeff Straney <straneyjeff@gmail.com>
2017-06-06 12:24:35 -0400

    created some test accounts. just be sure to insert the universities before the students to prevent constraint errors

--- fixtures.sql
+++ fixtures.sql
@@ -11,3 +11,16 @@ VALUES
 ("University of Michigan", "Michigan St", "KA", "33516", 0),
 ("Cornell University", "Ithaca Ln", "NY", "33444", 0),
 ("Yale University", "Yale Blvd", "CT", "06520", 0);
+
+-- all users have a password of 1234
+INSERT INTO users (first_name, last_name, email, uni_id, hash, user_name, role)
+VALUES
+("jeff", "straney", "jeff@gmail.com", 1, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "jstraney", "SA"),
+("joe", "bilabog", "bilabog@knights.ucf.edu", 1, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "jbilabog", "STU"),
+("robert", "doorstopper", "doorstopper@knights.ucf.edu", 1, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "rdoorstopper", "STU"),
+("robert", "sandcastle", "sandcastle@yaleppl.yale.edu", 6, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "rsandcastle", "STU"),
+("josh", "doorstopper", "doorstopper@noles.fsu.edu", 2, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "rsandcastle", "STU"),
+("jonas", "borg", "jborg@kansas.edu", 3, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "jonasborg", "STU"),
+("jenifer", "bosch", "jbosch@michigan.edu", 4, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "jbosch", "STU"),
+("travis", "tarmach", "travis@michigan.edu", 4, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "ttarmach", "ADM"),
+("sara", "mackley", "jeff@gmail.com", 1, "$2y$12$QJpIgFQA42Lrsk6qjRXZuOTg8MMu4r.zOXfRhL4fKMOZIe8RS/DXq", "smackley", "ADM");
