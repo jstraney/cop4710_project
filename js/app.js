b3fef7528073ccc8e184353739fe5355e3d557b3
Jeff Straney <straneyjeff@gmail.com>
2017-06-06 08:50:38 -0400

    added a javascript method to create a google map view of the event specifically for viewing. this is different than the map generated for editing/creating because you cannot click and drag the marker.

--- js/app.js
+++ js/app.js
@@ -11,6 +11,9 @@ var app = app || {};
     var lat = $("#lat");
     var lon = $("#lon");
 
+    defaultLat = 28.6024274;
+    defaultLon = -81.2000598;
+
     // select the element with id of map
     $("#map").locationpicker({
       // set defaults
@@ -18,14 +21,20 @@ var app = app || {};
       locationName: "Orlando, FL 32816, USA",
       // go knights!
       location: {
-        longitude: -81.2000598,
-        latitude: 28.6024274,
+        longitude: defaultLon,
+        latitude: defaultLat,
       },
       radius: 0,
       inputBinding: {
         longitudeOutput: $("#lon"),
-        latitudeInput: $("#lat"),
+        latitudeOutput: $("#lat"),
         locationNameInput: $("#location"),
+      },
+      oninitialized: function (component) {
+
+        lon.val(defaultLon);
+        lat.val(defaultLat);
+
       },
       // set event listener to change the lat and lon fields when the map is
       // changed.
@@ -35,5 +44,33 @@ var app = app || {};
       }
     });
   };
+
+  app.makeMapPresentation = function () {
+    // latitude and longitude fields of the form containing the map
+    var lat = $("#lat");
+    var lon = $("#lon");
+
+    // select the element with id of map
+    $("#map").locationpicker({
+      // set defaults
+      location: {
+        latitude: lat.val(),
+        longitude: lon.val(),
+      },
+      enableAutocomplete: true,
+      radius: 0,
+      inputBinding: {
+        longitudeInput: $("#lon"),
+        latitudeInput: $("#lat"),
+        locationNameInput: $("#location"),
+      },
+      markerDraggable: false,
+      oninitialized: function (component) {
+        console.log(component);
+      },
+
+    });
+
+  }
 
 })(app);
